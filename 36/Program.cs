36cebf461aaabd52e234d4d66f444a5a7b7a37ba
Andrey Volynets <voland.artist@gmail.com>
2022-10-03 03:35:58 +0400

    упростил подсчет суммы

--- 36/Program.cs
+++ 36/Program.cs
@@ -27,12 +27,9 @@ int[] getArray3DigitNunbers(int arrayLength)
 int getSumOddIndex(int[] arrayOddIndex)
 {
     int sum = 0;
-    for (int i = 0; i < arrayOddIndex.Length; i++)
+    for (int i = 1; i < arrayOddIndex.Length; i += 2)
     {
-        if (i % 2 != 0)
-        {
-            sum += arrayOddIndex[i];
-        }
+        sum += arrayOddIndex[i];
     }
     return sum;
 }
